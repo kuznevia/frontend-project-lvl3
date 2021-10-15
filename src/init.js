8541ff09b0579440d9fadf82d5d7e1a3bf240fe1
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2021-10-15 16:36:34 +0300

    adding form element and eventListener

--- src/init.js
+++ src/init.js
@@ -1,1 +1,10 @@
-export default () => console.log('HELLO!');
+const init = () => {
+  const form = document.getElementById('rss-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    console.log(formData.get('url'));
+  });
+};
+
+export default init;
