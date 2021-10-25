bf0b3cb94b87956aa1d0c3013bd1f131d402bbfc
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2021-10-25 21:12:16 +0300

    switching to step back

--- src/init.js
+++ src/init.js
@@ -88,7 +88,8 @@ const init = () => {
     },
   });
 
-  document.getElementById('rss-form').addEventListener('submit', (e) => {
+  const form = document.getElementById('rss-form');
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url');
